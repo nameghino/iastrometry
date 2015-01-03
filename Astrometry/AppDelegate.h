ba84862b7a486142a8461d5824046fdf02c17280
Nico Ameghino <nicolas.ameghino@gmail.com>
2015-01-03 17:27:54 -0300

    Initial Commit

---
+++ Astrometry/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  Astrometry
+//
+//  Created by Nicolas Ameghino on 1/3/15.
+//  Copyright (c) 2015 Nicolas Ameghino. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ Astrometry/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  Astrometry
+//
+//  Created by Nicolas Ameghino on 1/3/15.
+//  Copyright (c) 2015 Nicolas Ameghino. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
